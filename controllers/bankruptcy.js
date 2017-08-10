89fef41e35adb9b33c729986639ead72f694ada6
Parker <parkerproject@gmail.com>
2017-08-10 16:58:48 -0400

    updates

--- controllers/bankruptcy.js
+++ controllers/bankruptcy.js
@@ -1,11 +1,12 @@
-const { map, uniq } = require('lodash');
+const { map, uniq, orderBy } = require('lodash');
 const db = require('../helpers/db');
 
 module.exports = {
   index: {
     handler(request, reply) {
-      db.bankruptcy.find({}, (err, results) => {
-        let tabs = map(results, 'TAB');
+      db.bankruptcy.find({}, (err, data) => {
+        let tabs = map(data, 'TAB');
+        const results = orderBy(data, ['AMOUNT'], ['asc']);
         tabs = uniq(tabs);
         reply({ tabs, results });
       });
